cc2e76cf8b04d372c2e25c65157b98ee8a4f09e9
tdhonden <tenzin.dhonden@nomura.com>
2024-06-18 15:21:43 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,34 @@
 import streamlit as st
 import pandas as pd
-import streamlit_options_menu
+import matplotlib.pyplot as plt
+from pathlib import Path
 
+# Title of the App
+st.title('Simple CSV Data Chart with pathlib')
 
-# Create streamlit app
-st.title  ('Options Volatility Ratios')
+#Define the path to the CSV file using pathlib
 
-#Display Data
-st.write('Delta 25 and Delta 50 Ratios')
+data_file_path = Path(__file__).parent / 'data/delta50.csv'
+
+#Load data
+
+@st.cache_data
+def load_data(file_path):
+    data = pd.read_csv(file_path)
+    return data
+
+data = load_data(data_file_path)
+
+#display data
+st.write('## Data')
 st.write(data)
 
+#create a simple chart
+
+st.write('##chart')
+fig, ax = plt.subplots()
+ax.bar(data['Date'], data['Delta_1m'])
+ax.set_xlabel('Date')
+ax.set_ylabel('Delta_1m')
+
+st.pyplot(fig)
