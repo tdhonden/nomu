3d3c560f6f771fa204634a8d2f42596ff7cdd28e
tdhonden <tenzin.dhonden@nomura.com>
2024-06-18 15:01:35 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
+import pandas as pd
+import streamlit_options_menu
+from find-iv.py import *
 
 
-st.title  ('Hello, Streamlit!')
-st.write('Welcome to your first Streamlit app.')
+# Create streamlit app
+st.title  ('Options Volatility Ratios')
+
+#Display Data
+st.write('Delta 25 and Delta 50 Ratios')
+st.write(data)
 
